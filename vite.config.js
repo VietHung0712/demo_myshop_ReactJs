21cb50a7ed67111c2ef077f7623dfd75dece32ac
VietHung0712 <quyvuongbn@gmail.com>
2025-07-02 16:25:58 +0700

    Deploy

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: 'demo_myshop_ReactJs',
+  base: '/demo_myshop_ReactJs/',
   plugins: [react()],
 })
